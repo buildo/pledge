b017afe3c51a9df09bfe2395413a842651dfc3cb
Francesco <firefelix@gmail.com>
2016-10-09 17:15:05 +0200

    parse human date; improve success notifications

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import express from 'express';
 import request from 'request-promise';
 import bodyParser from 'body-parser';
 import _debug from 'debug';
+import human2date from 'date.js';
 import config from '../config.json';
 
 const INCOMING_WEBHOOK_URL = config.slack.incomingWebhookURL;
 
 const debug = _debug('pledge');
+
+const formatDate = date => {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  return `${date.getDate()} ${months[date.getMonth()]} at ${date.getHours()}:${date.getMinutes()}`;
+};
 
 const postOnSlack = json => request({
   json,
@@ -26,23 +33,29 @@ app.post('/newPledge', async ({ body: { text, user_name } }, res) => {
     const [, performer, pledge, deadline] = /(@[a-zA-Z0-9]+) (.+) by (.+)/.exec(text.trim()) || [];
 
     if (!performer) {
-      res.send('"Username" invalid. (@username [what] by [when])');
+      res.send('"Username" is missing. (@username [what] by [when])');
     } else if (!pledge) {
-      res.send('"What" invalid. (@username [what] by [when])');
+      res.send('"What" is missing. (@username [what] by [when])');
     } else if (!deadline) {
-      res.send('"When" invalid. (@username [what] by [when])');
+      res.send('"When" is missing. (@username [what] by [when])');
+    }
+
+    const parsedDeadline = human2date(deadline);
+
+    if (parsedDeadline.getTime() < Date.now()) {
+      res.send('"When" should be in the future');
     }
 
     const requester = `@${user_name}`;
 
     postOnSlack({
-      text: `${requester} added a pledge: "${pledge}"`,
+      text: `${requester} added a pledge: "${pledge} by ${deadline} (${formatDate(parsedDeadline)})"`,
       channel: performer,
       username: 'pledge',
       icon_emoji: ':dog:'
     });
 
-    res.send(`Successfully added pledge: "${pledge}"`);
+    res.send(`Successfully added pledge: "${pledge} by ${deadline} (${formatDate(parsedDeadline)})"`);
   } catch (err) {
     debug(err);
     res.status(500).json({ err });
