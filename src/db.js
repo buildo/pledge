b0c9055988c85710a30b1b6993f75d2b3b333220
Luca Cioria <vircheck@gmail.com>
2016-12-17 19:37:01 +0100

    add automatic migration script

--- src/db.js
+++ src/db.js
@@ -16,6 +16,39 @@ const createTables = () => {
   );
 };
 
+const migrateIfNeeded = async () => {
+  const latestVersion = 2;
+  // if there is no table schemaVersions, currentVersion is 1
+  let currentVersion = (await db.get(`
+    SELECT 1 FROM sqlite_master WHERE name ='schemaVersions' and type='table';
+  `) ? undefined : 1);
+  // if there is the schemaVersions table we can check the last version
+  if (typeof currentVersion === "undefined") {
+    currentVersion = (await db.get(`SELECT max(version) as v FROM schemaVersions`))['v'];
+  };
+  console.log(`starting pledge version ${currentVersion}`);
+  if (currentVersion === 1) {
+    // version 1 did not have a schemaVersions table, let's create it
+    await db.run(`
+      CREATE TABLE if not exists schemaVersions (
+        version INTEGER NOT NULL,
+        migrationDate INTEGER NOT NULL
+      );`
+    );
+    // add first version line with migrationDate = now
+    await db.run(`
+      INSERT INTO schemaVersions values (2, ?)
+    `, Date.now()
+    );
+    // perform migration v1 => v2
+    await db.run(`
+      ALTER TABLE pledges
+      ADD COLUMN expiredNotificationSent BOOLEAN NOT NULL DEFAULT 0`
+    );
+    console.log("migrated DB to version 2");
+  }
+};
+
 export const getList = async requester => {
   const requests = (await db.all(`
     SELECT id, requester, performer, content, deadline
@@ -79,4 +112,5 @@ export const deletePledge = pledgeId => {
 export const init = async () => {
   await db.open(config.db);
   await createTables();
+  await migrateIfNeeded();
 };
