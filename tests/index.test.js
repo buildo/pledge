60cf66b7688342ed14d51564e6882035a0b51205
Luca Cioria <vircheck@gmail.com>
2017-02-04 17:06:06 +0300

    completed test -> expired: notifies both at the right time, stays in list

--- tests/index.test.js
+++ tests/index.test.js
@@ -77,18 +77,22 @@ describe('app', () => {
         });
     });
 
-    it('notifies both requester and performer when a pledge has expired', () => {
+    it('expired: notifies both at the right time, stays in list', () => {
       const timezoneOffset = new Date().getTimezoneOffset();
       const interval = 10 * 60 * 60 * 1000; // 10 hours
       const adjInterval = interval + timezoneOffset * 60 * 1000;
       MockDate.set(0);
       return request(app).post('/slackCommand')
         .send('user_name=requester')
-        .send('text=@performer pledge content by today at 10am')
+        .send('text=@performer content by today at 10am')
         .expect(200)
         .then(async () => {
+          // number of notifications sent to both performer and requester
           const nExp = () =>
-            slack.postOnSlackMultipleChannels.mock.calls.filter((c) => c[0].text.match(/expired/)).length;
+            slack.postOnSlackMultipleChannels.mock.calls.filter((c) => {
+              return c[0].text.match(/expired/) && c[1].includes('@performer')
+                && c[1].includes('@requester') && c[1].length === 2;
+            }).length;
           // just before, no notifications
           MockDate.set(adjInterval - 1);
           await findNewNotifications();
@@ -101,6 +105,16 @@ describe('app', () => {
           MockDate.set(adjInterval + 999);
           await findNewNotifications();
           expect(nExp()).toBe(1);
+          // check that it's still present in list
+          return request(app).post('/slackCommand')
+            .send('user_name=requester')
+            .send('text=list')
+            .expect(200)
+            .then((res) => {
+              expect(typeof res.text).toBe('string');
+              // pledge is present
+              expect(res.text).toMatch('_@performer_ pledged to content *by 1 January at 10:00*');
+            });
         });
     });
 
