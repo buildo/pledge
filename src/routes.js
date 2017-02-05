8820d6d64491f0f25536f26207224ecae6634dac
Luca Cioria <vircheck@gmail.com>
2017-02-05 14:19:47 +0100

    adapted routes to work with bot and added oauth2

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import config from '../config.json';
 import bodyParser from 'body-parser';
 import _debug from 'debug';
 import human2date from 'date.js';
@@ -8,7 +9,21 @@ import * as slack from './slack';
 
 const debug = _debug('pledge');
 
-async function newPledge({ text, requester }) {
+const credentials = {
+  client: {
+    id: config.slack.clientId,
+    secret: config.slack.clientSecret
+  },
+  auth: {
+    tokenHost: 'https://slack.com',
+    tokenPath: '/api/oauth.access',
+    authorizePath: '/oauth/authorize'
+  }
+};
+
+const oauth2 = require('simple-oauth2').create(credentials);
+
+async function newPledge({ text, requester }, botAccessToken) {
   const [, performer, content, humanReadableDeadline] = /(@[a-zA-Z0-9]+) (.+) by (.+)/.exec(text.trim()) || [];
 
   if (!performer) {
@@ -24,21 +39,22 @@ async function newPledge({ text, requester }) {
   if (deadline.getTime() < Date.now()) {
     throw new Error('"When" should be in the future');
   }
-
-  await db.insertPledge({ requester, performer, content, deadline }).then(debug);
+  const teamId = (await db.getTeamByBotAccessToken(botAccessToken)).teamId;
+  await db.insertPledge({ teamId, requester, performer, content, deadline }).then(debug);
 
   await slack.postOnSlack({
     text: `${requester} asked you to "${content}" by ${humanReadableDeadline} (${formatDate(deadline)})`,
     channel: performer
-  });
+  }, botAccessToken);
 
   return `You asked ${performer} to "${content}" by ${humanReadableDeadline} (${formatDate(deadline)})`;
 }
 
-async function getPledgesList(requester) {
-  const { requests, pledges } = await db.getList(requester);
+async function getPledgesList(requester, botAccessToken) {
+  const teamId = db.getTeamByBotAccessToken(botAccessToken).teamId;
+  const { requests, pledges } = await db.getList(requester, teamId);
 
-  const baseURL = 'https://pledge.our.buildo.io';
+  const baseURL = config.domain;
 
   const myPledges = `*My pledges:*\n${pledges.map(p => `\n • ${p.content} _for ${p.requester}_ *by ${p.deadline}* <${baseURL}/deletePledge/${p.id}|delete> <${baseURL}/completePledge/${p.id}|complete>`)}`;
   const myRequests = `*My requests:*\n${requests.map(p => `\n • _${p.performer}_ pledged to ${p.content} *by ${p.deadline}* <${baseURL}/deletePledge/${p.id}|delete> <${baseURL}/completePledge/${p.id}|complete>`)}`;
@@ -52,7 +68,7 @@ export async function findNewNotifications() {
   expiredPledges.map(async p => {
     await slack.postOnSlackMultipleChannels({
       text: `pledge ${p.content} expired just now`
-    }, [p.requester, p.performer]);
+    }, [p.requester, p.performer], (await db.getTeam(p.teamId)).botAccessToken);
     await db.setExpiredNotificationAsSentOnPledge(p.id);
   });
 }
@@ -65,7 +81,24 @@ const router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post('/slackCommand', async ({ body: { text, user_name } }, res) => {
+router.get('/callback', async (req, res) => {
+  const code = req.query.code;
+  const options = { code };
+
+  oauth2.authorizationCode.getToken(options, async (error, result) => {
+    if (error) {
+      console.error('Access Token Error', error.message); // eslint-disable-line no-console
+      return res.json('Authentication failed');
+    }
+    await db.insertTeam(result.team_id, result.team_name, result.bot.bot_user_id,
+      result.bot.bot_access_token);
+    return res
+      .status(200)
+      .send('You have successfully installed pledge ;-)');
+  });
+});
+
+router.post('/slackCommand', async ({ body: { text, user_name, user_id, team_id } }, res) => {
   debug({ text, user_name });
   const requester = `@${user_name}`;
 
@@ -73,12 +106,13 @@ router.post('/slackCommand', async ({ body: { text, user_name } }, res) => {
     return res.status(422).send('command does not respect Slack POST format');
   }
 
+  const team = await db.getTeam(team_id);
   try {
     switch (text.trim()) {
       case 'list':
-        return res.send(await getPledgesList(requester));
+        return res.send(await getPledgesList(requester, team.botAccessToken));
       default:
-        return res.send(await newPledge({ text, requester }));
+        return res.send(await newPledge({ text, requester }, team.botAccessToken));
     }
   } catch (err) {
     debug(err);
