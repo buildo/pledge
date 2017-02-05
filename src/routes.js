131b51c9192fae4e2803f68e026e8eee1eb05adb
Luca Cioria <vircheck@gmail.com>
2017-02-05 14:21:34 +0100

    added route add-to-slack which shows the add to slack button in a browser

--- src/routes.js
+++ src/routes.js
@@ -150,4 +150,22 @@ router.get('/completePledge/:pledgeId', async ({ params: { pledgeId } }, res) =>
   }
 });
 
+router.get('/add-to-slack', async (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  const html = `
+    <body style="width:100%">
+      <div style="margin-top: 100px;">
+        <div style="margin:auto; text-align: center; margin-bottom: 50px; font-family: sans-serif; font-size: 40px; font-weight: bold; color: #333;">
+          Install pledge in your Slack team
+        </div>
+        <a style="display: block; margin: auto; width: 139px;" href="https://slack.com/oauth/authorize?scope=bot,commands&client_id=${config.slack.clientId}">
+          <img alt="Add to Slack" height="40" width="139"
+            src="https://platform.slack-edge.com/img/add_to_slack.png"
+            srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+        </a>
+      </div>
+    </body>`;
+  return res.send(html);
+});
+
 export default router;
